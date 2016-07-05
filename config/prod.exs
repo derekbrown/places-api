e126a8d795359460f276f568c4731e7b2c82372a
Derek Brown <derek@derek.am>
2016-07-05 17:06:30 -0400

    Prepare api for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :places, Places.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
 
 config :places, Places.Repo,
-  adapter:Ecto.Adapters.Postgres,
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20
 
